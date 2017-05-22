45e2599e5dc348795d71ab06f27ce616a93a4787
Tamara Jost <facebookoftjo@gmail.com>
2017-05-21 21:43:13 -0300

    show what tickets a user bought on the show user site

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])
+    @user = current_user
     @organizer = @user.organizer
+    @purchases = @user.purchases
 
 
   end
