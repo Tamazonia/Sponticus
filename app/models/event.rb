1c81baaf685561dd271429b8f91e3246fb57a9b7
Tamara Jost <facebookoftjo@gmail.com>
2017-06-08 10:22:39 -0300

    validation of event fields

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :organizer
   has_many :tickets, dependent: :destroy
+  validates :event_name, :event_address_street, :event_address_postalcode, :event_address_city, :date, :event_time, :event_category, :event_description, presence: true
 
 end
