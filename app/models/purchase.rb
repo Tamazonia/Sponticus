e8d8c7d28f0354ced26a454b538d7f7edd3b715d
Tamara Jost <facebookoftjo@gmail.com>
2017-07-18 18:03:27 -0300

    email confirmation to buyer

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -3,11 +3,16 @@ class Purchase < ApplicationRecord
   belongs_to :ticket
   validates :amount_tickets_purchased, numericality: { only_integer: true, greater_than: 0 }
 
-  after_create :send_sold_email
+  after_create :send_sold_email, :send_bought_email
+
   private
+
   def send_sold_email
     UserMailer.sold(self.ticket.event.organizer, self).deliver_now
+  end
 
+   def send_bought_email
+    UserMailer.bought(self.user, self).deliver_now
   end
 
 end
