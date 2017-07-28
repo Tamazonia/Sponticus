ecff60eb6ebf24c9835078f0cc87cb54edf1a937
Tamara Jost <facebookoftjo@gmail.com>
2017-07-28 19:03:03 -0300

    front index

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Purchase.destroy_all
+Order.destroy_all
 Ticket.destroy_all
 Event.destroy_all
 Organizer.destroy_all
