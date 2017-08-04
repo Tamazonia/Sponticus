3898df102e4bf6e1ddc45ffe384f97763b2b6fdf
Tamara Jost <facebookoftjo@gmail.com>
2017-08-04 11:39:10 -0300

    email delivery errors true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "sponticus.herokuapp.com" }
     config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
   # Ignore bad email addresses and do not raise email delivery errors.
