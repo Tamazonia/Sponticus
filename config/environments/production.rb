9ede00b7e518033e2c3adce16af9faee0abd8c65
Tamara Jost <facebookoftjo@gmail.com>
2017-08-04 11:23:31 -0300

    change in email for gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,9 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "sponticus.herokuapp.com" }
+    config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
